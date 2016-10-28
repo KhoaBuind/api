e3b2b70067e6f91e9050d4a7ee085ebf7dd33767
khoa.bui <khoa.bui@hotdeal.vn>
2016-10-28 15:46:13 +0700

    fix paginig

--- json.php
+++ json.php
@@ -3,11 +3,10 @@ header('Content-Type: application/json');
 include "dbconfig.php";
 $page = 1;
 $limit  = 10;
-$offset = ($page - 1) * $limit;
-
 
 if(isset($_GET['page'])){
 	$page = $_GET['page'] > 0 ? $_GET['page'] : 1;
 }
+$offset = ($page - 1) * $limit;
 $result = selectData("food",array(), array(), 0, $offset,$limit);
 echo json_encode(array('restaurants' => $result));
