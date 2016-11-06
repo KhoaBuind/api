a69b6ff9d93c2eff70ac4f3100ec08cfc2dde64b
khoa.bui <khoa.bui@hotdeal.vn>
2016-11-06 20:58:56 +0700

    config production

--- config/production/config.php
+++ config/production/config.php
@@ -9,7 +9,7 @@ return
 $config =
 [
     'database'  => [
-        'host'     => '54.169.255.186',
+        'host'     => 'localhost',
         'username' => 'root',
         'password' => '123456',
         'dbname'   => 'food',
